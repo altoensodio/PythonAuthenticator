efaf4eb038280b2a7202ae858ebd4e71034ebe1a
altoensodio <tomaszavala@protonmail.com>
2024-04-14 15:45:35 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,100 @@
+import pyotp
+import json
+from os.path import exists
+import typer
+from tabulate import tabulate
+
+app = typer.Typer()
+
+JSON_PATH = "keys.json"
+
+def exists_in_json(a):
+    with open(JSON_PATH) as f:
+        data = json.load(f)
+    if a in str(data):
+        return True
+    else:
+        return False
+
+
+def verify_json():
+    if exists(JSON_PATH):
+        pass
+    else:
+        try:
+            file = open(JSON_PATH, "w")
+            file.write('{"example": {"key": "base32secret3232","type": "time"}}')
+            file.close()
+        except Exception as e:
+            print("Unexpected error: {}".format(e))
+
+
+@app.command()
+def show_code(
+    name: str,
+    e: bool = typer.Option(False, help="Show TOTP code of a key outside key file."),
+):
+    if e:
+        try:
+            totp = pyotp.TOTP('{}'.format(name))
+            print(totp.now())
+        except Exception as e:
+            print("Error: {}".format(e))
+    else:
+        try:
+            if exists_in_json(name) == True: 
+                data = json.load(open(JSON_PATH))
+                totp = pyotp.TOTP(data[name]["key"])
+                print(totp.now())
+            else:
+                print('"{}" not in key file.')
+        except Exception as e:
+            print("Error: {}".format(e))
+
+@app.command()
+def add_key(name: str, key: str, ftype: str):
+    try:
+        verify_json()
+        if exists_in_json(name) == False:
+            y = {"{}".format(name):{"key": "{}".format(key),"type": "{}".format(ftype)}}
+            with open(JSON_PATH) as f:
+                data = json.load(f)
+            data.update(y)
+            with open(JSON_PATH, 'w') as f:
+                json.dump(data, f)
+        else:
+            print('"{}" already in key file.'.format(name))
+    except Exception as e:
+        print("Unexpected error: {}".format(e))
+
+
+@app.command()
+def del_key(name: str):
+    try:
+        verify_json()
+        if exists_in_json(name) == True:
+            data = json.load(open(JSON_PATH))
+            data.pop(name)
+            with open(JSON_PATH, "w") as f:
+                json.dump(data, f)
+        else:
+            print('"{}" is not in key file.'.format(name))
+
+    except Exception as e:
+          print("Unexpected error: {}".format(e))
+
+
+@app.command()
+def keys():
+    verify_json()
+    data = json.load(open(JSON_PATH))
+    key_list = [["Name", "Code"]]
+    for i in data:
+        key_list.append([i, pyotp.TOTP(data[i]["key"]).now()])
+    
+    table = tabulate(key_list,headers='firstrow', tablefmt="fancy_grid")
+    print(table)
+
+
+if __name__ == "__main__":
+    app()
